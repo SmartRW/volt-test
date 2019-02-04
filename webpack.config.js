43eb98f45dfb620de84cc5b4ce3d8f7c5b9754ce
SmartRW <7980900@gmail.com>
2019-02-04 20:22:04 +0300

    update webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,47 +1,45 @@
-'use strict';
-
-var path = require('path');
-var webpack = require('webpack');
-var HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require('path');
+const webpack = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   devtool: 'eval-source-map',
   entry: [
     'webpack-hot-middleware/client?reload=true',
-    path.join(__dirname, 'src/app.jsx')
+    path.join(__dirname, 'src/app.jsx'),
   ],
   resolve: {
-    extensions: ['.js', '.jsx', '.css']
+    extensions: ['.js', '.jsx', '.css'],
   },
   output: {
     path: path.join(__dirname, 'dist/public/'),
-    publicPath: '/assets/'
+    publicPath: '/assets/',
   },
   plugins: [
     new HtmlWebpackPlugin({
       template: 'src/index.tpl.html',
       inject: 'body',
-      filename: 'index.html'
+      filename: 'index.html',
     }),
     new webpack.optimize.OccurrenceOrderPlugin(),
     new webpack.HotModuleReplacementPlugin(),
     new webpack.NoEmitOnErrorsPlugin(),
     new webpack.DefinePlugin({
-      'process.env.NODE_ENV': JSON.stringify('development')
-    })
+      'process.env.NODE_ENV': JSON.stringify('development'),
+    }),
   ],
   module: {
     rules: [{
       test: /\.jsx?$/,
       exclude: /node_modules/,
-      use: 'babel-loader'
+      use: 'babel-loader',
     }, {
       test: /\.css$/,
-      use: ['style-loader', 'css-loader']
+      use: ['style-loader', 'css-loader'],
     },
     {
       test: /\.(woff|woff2|eot|ttf|otf|svg)$/,
-      use: 'file-loader'
-    }]
-  }
+      use: 'file-loader',
+    }],
+  },
 };
