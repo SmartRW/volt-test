a5886738171aa647098670d239beab77cd238fef
roman.makarov <7980900@gmail.com>
2019-02-05 14:14:51 +0300

    add heroku-postbuild script

--- server/server.js
+++ server/server.js
@@ -305,7 +305,6 @@ const isDeveloping = process.env.NODE_ENV !== 'production';
 const port = isDeveloping ? 3000 : process.env.PORT;
 
 if (isDeveloping) {
-  console.log('is developing!!!');
   const compiler = webpack(config);
   const middleware = webpackMiddleware(compiler, {
     publicPath: config.output.publicPath,
@@ -327,7 +326,6 @@ if (isDeveloping) {
     res.end();
   });
 } else {
-  console.log('is production!!!');
   app.use(express.static(path.join(__dirname, 'public')));
   app.get('*', function response(req, res) {
     res.sendFile(path.join(__dirname, 'public/index.html'));
